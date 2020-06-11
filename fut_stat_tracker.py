d72aba8a533e5d2e4ef3eab99ab8531355321a4b
VIctor J. Ramos <victorjramos2@gmail.com>
2020-06-10 21:02:14 -0700

    done need to comment
    added the last graphs  everything seem to work properly

--- fut_stat_tracker.py
+++ fut_stat_tracker.py
@@ -130,10 +130,13 @@ def graph1(self):
         modeappend = infosplit[2]
         etappend = infosplit[3]
         penplayeddata = infosplit[4]
-        penscoreappend = infosplit[5]
-        penscoreoppappend = infosplit[6]
+        penscoreappend = infosplit[5].replace(' ','')
+        penscoreoppappend = infosplit[6].replace(' ','')
         wltappend = infosplit[7]
         wolappend = infosplit[8]
+
+        #penscoreappend = list(filter(("").__ne__,penscoreappend))
+        #penscoreoppappend = list(filter(("").__ne__,penscoreoppappend))
 
         #scoredata.append(scoreappend)
         scoregraph.append(scoreappend)
@@ -145,6 +148,10 @@ def graph1(self):
         penscoreopp.append(penscoreoppappend)
         hiedata.append(wltappend)
         resultdata.append(wolappend)
+    
+        
+        penscore = list(filter(("").__ne__,penscore))
+        penscoreopp = list(filter(("").__ne__,penscoreopp))
         
     #print(scoregraph)
     #print(scoregraphopp)
@@ -161,7 +168,7 @@ def graph1(self):
     #graph = list(filter(("").__ne__,graph))
 
     o = graphseleted.get()
-    if o == "Goal":
+    if o == "Goals":
         plt.close()
         newg = [int(i) for i in scoregraph]
         newop = [int(j) for j in scoregraphopp]
@@ -209,13 +216,44 @@ def graph1(self):
         arr = np.array([w,l,t])
         plt.pie(arr,labels=labels, autopct='%1.1f%%')
         plt.show()
+    elif o == "Pens":
+        plt.close()
+        newpen = [int(i) for i in penscore]
+        newpenopp = [int(j) for j in penscoreopp]
+        f, (ax1, ax2) = plt.subplots(1,2)
+        labels, counts = np.unique(newpen,return_counts=True)
+        labels2, counts2 = np.unique(newpenopp,return_counts=True)
+        ax1.bar(labels,counts,align='center')
+        ax2.bar(labels2,counts2,align='center')
+        plt.gca().set_xticks(labels)
+        ax1.set(xlabel='Pens Score',ylabel='# of games')  
+        ax2.set(xlabel='Pens Score',ylabel='# of games')
+        ax1.set_title("My Pens Goals")
+        ax2.set_title("Opponents Pens Goals")
+        plt.show()
+    elif o == "How it Ended":
+        plt.close()
+        n = 0
+        rq = 0
+        dc = 0
+        for a in hiedata:
+            if a == " Normal":
+                n = n + 1
+            elif a == " Rage Quit":
+                rq = rq + 1
+            elif a == " DC":
+                dc = dc + 1 
+        labels = 'Normal', 'Rage Quit', 'Tie'
+        arr = np.array([n,rq,dc])
+        plt.pie(arr,labels=labels, autopct='%1.1f%%')
+        plt.show() 
     
     conn.commit()
     conn.close()
     
 
-title = Label(root, text="Fut Stat Traker")
-title.grid(row=0, column=0,columnspan=4)
+#title = Label(root, text="Fut Stat Traker")
+#title.grid(row=0, column=0,columnspan=4)
 
 titlescore = Label(root, text="Score: ")
 titlescore.grid(row=1, column=0)
@@ -225,54 +263,54 @@ iuserscore.grid(row=1, column=1)
 opponetscore = Entry(root,width=2)
 opponetscore.grid(row=1, column=3)
 
-resultl = Label(root,text="Result: ").grid(row=1,column=4)
+resultl = Label(root,text="Result: ").grid(row=3,column=0)
 clickedresult = StringVar()
 clickedresult.set("Win")
 resultm = OptionMenu(root, clickedresult, "Win", "Lose", "Tie")
-resultm.grid(row=1,column=5)
+resultm.grid(row=3,column=1)
 
 clicked = StringVar()
 clicked.set("WL")
-gamemode = Label(root,text="Mode: ").grid(row=1,column=6)
+gamemode = Label(root,text="Mode: ").grid(row=3,column=2)
 gameselection = OptionMenu(root, clicked, "WL", "Rivals")
-gameselection.grid(row=1,column=7)
+gameselection.grid(row=3,column=3)
 
 etv = IntVar()
 et = Checkbutton(root, text="Extra Time",variable=etv)
-et.grid(row=2,column=0)
+et.grid(row=2,column=4,sticky=W)
 
 pen = IntVar()
 penc = Checkbutton(root, text="Penaties", variable=pen)
-penc.grid(row=2,column=1)
+penc.grid(row=2,column=0)
 
 
 penuserscore = Entry(root, width = 2)
-penuserscore.grid(row=2,column=2)
+penuserscore.grid(row=2,column=1)
 ctt = Label(root,text=" : ")
-ctt.grid(row=2,column=3)
+ctt.grid(row=2,column=2)
 penoppscore = Entry(root, width = 2)
-penoppscore.grid(row=2, column=4)
+penoppscore.grid(row=2, column=3)
 
 gameend = StringVar()
 gameend.set("Normal")
 ge = Label(root,text="How it ended: ")
-ge.grid(row=2, column=5)
+ge.grid(row=1, column=4)
 ges = OptionMenu(root, gameend, "Normal", "Rage Quit", "DC")
-ges.grid(row=2, column=6)
+ges.grid(row=1, column=5)
 
 submitbtn = Button(root,text="Submit", command=submit)
-submitbtn.grid(row=3,column=0,columnspan=7)
+submitbtn.grid(row=3,column=4,columnspan=2)
 
 showbtn = Button(root,text="Show",command=show)
-showbtn.grid(row=4,column=0)
+showbtn.grid(row=3,column=6,sticky=W)
 
 delete_lastrecourd = Button(root,text="Delete Last Entry",command=delete)
-delete_lastrecourd.grid(row=4,column=1)
+delete_lastrecourd.grid(row=1,column=6)
 
 graphseleted = StringVar()
 graphseleted.set("Select Data to Graph")
-grhsel=OptionMenu(root, graphseleted, "Goal", "% Mode Played", "W/L/T Ratio",command=graph1)
-grhsel.grid(row=5,column=0)
+grhsel=OptionMenu(root, graphseleted, "Goals", "% Mode Played", "W/L/T Ratio","Pens","How it Ended",command=graph1)
+grhsel.grid(row=2,column=5,columnspan=2,sticky=W)
 
 conn.commit()
 
